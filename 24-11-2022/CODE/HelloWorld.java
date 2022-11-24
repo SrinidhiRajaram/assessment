62147d8e8c675c9374ec634d33336fa66ea9b7fc
SrinidhiRajaram <118958442+SrinidhiRajaram@users.noreply.github.com>
2022-11-24 12:04:24 +0530

    Add files via upload

---
+++ 24-11-2022/CODE/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld
+{
+	public static void main(String []args)
+	{
+		System.out.println(" I am Srinidhi ");
+		System.out.println(" I am completed BCA ");
+		System.out.println(" Address: keelkattalai ");
+		System.out.println(" Number: 7777777777");
+	}	
+
+}
+
